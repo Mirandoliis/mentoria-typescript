f338ac49da8dc590b2a0b6eb298ea141f4ed5aea
Mirandoliis <107009148+Mirandoliis@users.noreply.github.com>
2022-08-02 13:57:16 -0300

    Desafios Typescript
    Segue os 3 primeiros desafios.

---
+++ Desafios TS/desafio2.ts
@@ -0,0 +1,31 @@
+interface Pessoa {
+    nome: string,
+    idade: number,
+    Profissao: string
+};
+
+let pessoa1: Pessoa = {
+    nome: "maria",
+    idade: 29,
+    Profissao: "atriz"
+};
+
+
+let pessoa2: Pessoa = {
+    nome: "roberto",
+    idade: 19,
+    Profissao: "Padeiro"
+};
+
+
+let pessoa3 = {
+    nome: "laura",
+    idade: "32",
+    Profissao: "Atriz"
+};
+
+let pessoa4 = {
+    nome: "carlos",
+    idade: 19,
+    Profissao: "padeiro"
+};
